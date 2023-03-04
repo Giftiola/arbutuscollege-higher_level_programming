680f92c2dd2bd05d602939985add7ba69481c7f0
Giftiola <52719101+Giftiola@users.noreply.github.com>
2023-03-03 22:06:39 -0800

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+# 6-print_matrix_integer.py
 
 def print_matrix_integer(matrix=[[]]):
-    """
-    prints a matrix of integers to STDOUT
-    """
-    for row in matrix:
-        for i in range(len(row)):
-            if i != row_len - 1:
-                print("{:d}".format(row[i]), end=' ')
-            else:
-                print("{:d}".format(row[i]), end='')
-        print()
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
