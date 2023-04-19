9987ecac4c297717ebb758dc1977d3a5b379b2ac
Giftiola <52719101+Giftiola@users.noreply.github.com>
2023-04-18 23:37:52 -0700

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# 7-add_item.py
 """Add all arguments to a Python list and save them to a file."""
-from os import path
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+import sys
 
-if path.exists('add_item.json'):
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
+
+    if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add_item.json')
 else:
     obj_json_file = []
